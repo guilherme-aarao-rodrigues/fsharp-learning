18a02cae3b288d9438d4040b1865f28a4a0a494c
Guilherme Rodrigues <gro@phatconsulting.de>
2023-05-04 15:11:49 +0200

    wip

--- scripts/exercises-async.fsx
+++ scripts/exercises-async.fsx
@@ -1,4 +1,4 @@
-//Exercises from Kip Eason's 'Stylish F#' book
+//Exercises from Kip Eason's 'Stylish F#' book, Chapter 10
 
 //This section contains exercises to help you get used to translating code into an
 //asynchronous world.
@@ -27,6 +27,17 @@ module Server =
             return Random.string ()
         }
 
+    let GetStringAsync (id: int) =
+        // id is unused
+        task {
+            do! Async.Sleep(500)
+            return Random.string ()
+        }
+
+module Async =
+    let ParallelThrottled maxDegreeOfParallelism tasks =
+        Async.Parallel(tasks, maxDegreeOfParallelism)
+
 module Consumer =
     let GetData (count: int) =
         let strings =
@@ -34,50 +45,85 @@ module Consumer =
 
         strings |> Array.sort
 
-    let GetDataAsync (count: int) =
+    let AsyncGetDataParallel (count: int) =
         async {
             let! strings =
-                (Array.init count (fun i -> Server.AsyncGetString i))
-                |> Async.Parallel
+                (Array.init count (fun i -> Server.AsyncGetString i)) //Fork
+                |> Async.Parallel //Join
 
             return (strings |> Array.sort)
         }
 
-let logUsageError() =
+    let AsyncGetDataParallelThrottled (count: int) (maxDegreeOfParallelism: int) =
+        async {
+            let! strings =
+                (Array.init count (fun i -> Server.AsyncGetString i)) //Fork
+                |> Async.ParallelThrottled maxDegreeOfParallelism //Join
+
+            return (strings |> Array.sort)
+        }
+
+    let GetDataAsyncSequential (count: int) =
+        async {
+            let! strings =
+                (Array.init count (fun i -> Server.AsyncGetString i))
+                |> Async.Sequential
+
+            return (strings |> Array.sort)
+        }
+
+let logUsage () =
     printfn
         @"This exercise takes 1 argument. Usage: 
     - dotnet fsi exercises-async.fsx S: Run synchronous solution
     - dotnet fsi exercises-async.fsx AP: Run asynchronous parallel solution
+    - dotnet fsi exercises-async.fsx APT: Run asynchronous parallel throttled (2) solution
     - dotnet fsi exercises-async.fsx AS: Run asynchronous sequential solution"
 
 let args = fsi.CommandLineArgs |> Array.tail
-async {
-if (args.Length <> 1) then
-    logUsageError()
-    return 1
-else
-    let sw = System.Diagnostics.Stopwatch()
-    sw.Start()
-    match args.[0] with
-    | "S" -> 
-        let results = Consumer.GetData 10
-        printfn "Printing results (Synchronous case)"
-        results |> Array.iter (fun s -> printfn "%s" s)
-        printfn "That took %ums" sw.ElapsedMilliseconds
-        return 0
-    | "AP" -> 
-        let! results = Consumer.GetDataAsync 10
-        printfn "Printing results (Asynchronous case - Parallel)"
-        results |> Array.iter (fun s -> printfn "%s" s)
-        printfn "\nThat took %ums" sw.ElapsedMilliseconds
-        return 0
-    | _ -> 
-        logUsageError()
-        return 1
 
-}|> Async.RunSynchronously //since its the only task in main, it makes no difference
-      
-        
+async {
+    if (args.Length <> 1) then
+        logUsage ()
+        return 1
+    else
+        let sw = System.Diagnostics.Stopwatch()
+        sw.Start()
+
+        match args.[0] with
+        | "S" ->
+            let results = Consumer.GetData 10
+            printfn "Printing results (Synchronous case)"
+            results |> Array.iter (fun s -> printfn "%s" s)
+            printfn "That took %ums" sw.ElapsedMilliseconds
+            return 0
+        | "AP" ->
+            let! results = Consumer.AsyncGetDataParallel 10
+            printfn "Printing results (Asynchronous case - Parallel)"
+            results |> Array.iter (fun s -> printfn "%s" s)
+            printfn "\nThat took %ums" sw.ElapsedMilliseconds
+            return 0
+        | "APT" ->
+            let! results = Consumer.AsyncGetDataParallelThrottled 10 2
+            printfn "Printing results (Asynchronous case - Parallel)"
+            results |> Array.iter (fun s -> printfn "%s" s)
+            printfn "\nThat took %ums" sw.ElapsedMilliseconds
+            return 0
+
+        | "AS" ->
+            let! results = Consumer.GetDataAsyncSequential 10
+            printfn "Printing results (Asynchronous case - Parallel)"
+            results |> Array.iter (fun s -> printfn "%s" s)
+            printfn "\nThat took %ums" sw.ElapsedMilliseconds
+            return 0
+        | _ ->
+            logUsage ()
+            return 1
+
+}
+|> Async.RunSynchronously //since its the only task in main, it makes no difference
+
+
 
 //If you run the code, you’ll notice that this operation takes over 5 seconds to get ten results.
 //Change the Consumer.GetData() function so that it is asynchronous and so that it runs all
